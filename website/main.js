497a89df1027aa8cf26f372ae17bd590468c401b
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-04-06 00:09:06 -0300

    user can now only input numbers on the editable inputs

--- website/main.js
+++ website/main.js
@@ -1,3 +1,34 @@
+function run(element){
+    let input1
+    let input2
+    let input3
+    let id
+    try{
+        id = element.id;
+        input1 = parseInt(document.getElementById(id + 'Input1').textContent)
+        input2 = parseInt(document.getElementById(id + 'Input2').textContent)
+        input3 = parseInt(document.getElementById(id + 'Input3').textContent)
+    }
+    catch{}
+    document.getElementById(id + 'Output').innerHTML = "&nbsp&nbsp > " + window[id](input1, input2, input3)
+}
+
+//event listener to prevent user from inputing strings instead of numbers and stuff like that
+function addEventListeners(){
+    let inputElements = document.getElementsByClassName("input")
+
+    let eventListenerFunction = function(e){if (isNaN(String.fromCharCode(e.which))) e.preventDefault();}
+    
+    for(let i = 0; i < inputElements.length; i++){
+        inputElements[i].addEventListener("keypress", eventListenerFunction);
+    }
+}
+
+//run this when page is loaded
+addEventListeners()
+
+
+
 function helloWorld(input){
     return input
 }
@@ -17,20 +48,4 @@ function factorial(x){
 
 function inRange(min, max, x){
     return x >= min && x <= max
-}
-
-
-function run(element){
-    let input1
-    let input2
-    let input3
-    let id
-    try{
-        id = element.id;
-        input1 = parseInt(document.getElementById(id + 'Input1').textContent)
-        input2 = parseInt(document.getElementById(id + 'Input2').textContent)
-        input3 = parseInt(document.getElementById(id + 'Input3').textContent)
-    }
-    catch{}
-    document.getElementById(id + 'Output').innerHTML = "&nbsp&nbsp > " + window[id](input1, input2, input3);
 }
